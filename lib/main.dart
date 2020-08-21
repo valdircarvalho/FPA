454938d2d9efa671be74e2d4df4c90bccb1ee31e
valdircarvalho <valdir.carv@gmail.com>
2020-08-21 11:16:58 -0300

    StartCommit by test.

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter TESTE_'),
+      home: MyHomePage(title: 'Flutter TESTE'),
     );
   }
 }
